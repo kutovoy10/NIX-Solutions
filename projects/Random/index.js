9a3e2c64891419aab5b625e113e07d97ebd8b353
kutovoy10 <44513599+kutovoy10@users.noreply.github.com>
2021-06-09 19:23:51 +0300

    add project 'random'

---
+++ projects/Random/index.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector('.random__btn');
+const result = document.querySelector('.random__result');
+
+btn.addEventListener('click', () => {
+    let quantity = +document.querySelector('.random__quantity').value;
+    let min = +document.querySelector('.random__min').value;
+    let max = +document.querySelector('.random__max').value;
+
+    if (min < max && quantity > 0) {
+        generateNumber(quantity, min, max);
+    } else {
+
+    }
+})
+
+function generateNumber(quantity, min, max) {
+    let listRandom = [];
+    let out = ``;
+
+    for (let i = 0; i < quantity; i++) {
+        listRandom.push(Math.floor(min + Math.random() * (max + 1 - min)));
+    }
+
+    for (let j in listRandom) {
+        out += listRandom[j] + `, `;
+    }
+
+    result.innerHTML = out;
+    result.style.display = 'block';
+}
